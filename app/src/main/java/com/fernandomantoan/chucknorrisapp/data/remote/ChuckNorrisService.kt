de0956cd6d347cbd5b564af119fc589fbe7fa1be
Fernando Mantoan <contato@fernandomantoan.com>
2021-09-26 15:14:48 -0300

    Removing unused Service method

--- app/src/main/java/com/fernandomantoan/chucknorrisapp/data/remote/ChuckNorrisService.kt
+++ app/src/main/java/com/fernandomantoan/chucknorrisapp/data/remote/ChuckNorrisService.kt
@@ -1,16 +1,12 @@
 package com.fernandomantoan.chucknorrisapp.data.remote
 
 import com.fernandomantoan.chucknorrisapp.entity.ChuckNorrisResponse
-import com.fernandomantoan.chucknorrisapp.entity.Fact
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ChuckNorrisService {
     @GET("jokes/categories")
     suspend fun getCategories(): List<String>
-
-    @GET("jokes/random")
-    suspend fun getFactsFromCategory(@Query("category") category: String): List<Fact>
 
     @GET("jokes/search")
     suspend fun getFactsFromTextSearch(@Query("query") textSearch: String): ChuckNorrisResponse
